7a9f6f28ae44dec06bfa9c2c4882bebc9de85a5f
pashalan <pashalan@mail.ru>
2022-02-16 22:54:18 +0300

    Initial commit

---
+++ src/ru/io/FileAPP.java
@@ -0,0 +1,31 @@
+package ru.io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+public class FileAPP {
+    public static void main(String[] args) throws IOException {
+        File f = new File("file/personDB.txt");
+        if (f.exists()){
+            File parentDir = new File(f.getParent());
+            MyFileNameFilter filter = new MyFileNameFilter();
+            for (File cur : parentDir.listFiles(filter)){
+                System.out.println(cur.getName());
+                if (cur.isDirectory()){
+                    System.out.println ("Представлен каталог");
+                } else {
+                    System.out.println ("Представлен файл");
+
+                }
+
+            }
+        } else {
+            System.out.println("File created: " + f.getName());
+            f.createNewFile();
+
+        }
+
+
+    }
+}
---
+++ src/ru/io/basic/IORunner.java
@@ -0,0 +1,11 @@
+package ru.io.basic;
+
+public class IORunner {
+
+    public static void main(String[] args) {
+        System.out.println();
+        
+    }
+
+
+}
