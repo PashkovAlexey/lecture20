c9b0a38ab2e8c7548d4f8f7b262f06b2d193ce33
pashalan <pashalan@mail.ru>
2022-02-17 23:15:01 +0300

    some changes

--- src/ru/io/basic/IORunner.java
+++ src/ru/io/basic/IORunner.java
@@ -1,10 +1,52 @@
 package ru.io.basic;
 
+import ru.io.FileAPP;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
 public class IORunner {
 
-    public static void main(String[] args) {
-        System.out.println();
-        
+    public static void main(String[] args) throws IOException {
+        FileReader fr = null;
+        FileWriter fw = null;
+        try {
+            Scanner sc = new Scanner(System.in);
+            File f = new File("file/personDB.txt");
+
+            fr = new FileReader(f);
+            StringBuffer s = new StringBuffer();
+
+            int i = 0;
+            while ((i = fr.read()) != -1){
+                s.append((char)i);
+            }
+            System.out.println(s);
+
+            File f2 = new File("file/personDB2.txt");
+            if (f2.exists()) f2.createNewFile();
+
+            fw = new FileWriter(f2, true);
+            fw.append(sc.nextLine());
+
+            fw.flush();
+
+            System.out.println("Работа завершена успешно");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            System.out.println("Мы поймали ошибку");
+
+        } finally {
+            System.out.println("Всегда исполняюсь");
+            fr.close();
+            fw.close();
+        }
     }
 
 
